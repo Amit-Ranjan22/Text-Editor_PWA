155ecf033a0b1cdf22ce62353c2984cd2d3cceec
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-31 17:04:15 -0500

    define the name of the cache storage and plugins which cache response with headers to max-age of 30 days in registerRoute() inside src-sw.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -29,4 +29,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
+
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ]
+  })
 );
