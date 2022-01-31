2405420671a2b463ee93ace43cef604e3da7d359
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-01-31 16:02:01 -0500

    Add logic to accept some content and add it to the database in database.js in js folder

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,19 +1,33 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("edits", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("edits")) {
+        console.log("edits database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("edits", { keyPath: "id", autoIncrement: true });
+      console.log("edits database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const editsDb = await openDB("edits", 1);
+
+  const tx = editsDb.transaction("edits", "readwrite");
+
+  const store = tx.objectStore("edits");
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+
+  result
+    ? console.log("🚀 - Data saved to the database", result.value)
+    : console.error("putDb not implemented");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
